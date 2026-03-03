99f60908f243467c56c628400dd6d73c852fc891
hello <mine@example.com>
2026-03-03 16:54:48 +0800

    fucking chrome-type

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,1 +1,11 @@
 console.log('hlllo')
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("服务已启动，正在连接外部控制源...");
+  
+  // 选择一种模式启动
+  startPolling(); 
+  // startWebSocket(); // 如果需要实时性，取消注释并配置服务器
+});
+
+
+
